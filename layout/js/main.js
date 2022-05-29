3579df0779a33c4060061925320cac832adea48e
Artyom Svirin <clown1337@gmail.com>
2022-05-29 21:55:36 +0300

    fa

---
+++ layout/js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.service-content__sidebar__slider').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-angle-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-angle-right"></i></button>',
+
+    });
+
+});
